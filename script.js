a335fa5450af85b193a1a56000014f53a3bf5eb4
Mouez98 <mouez.matoui@gmail.com>
2021-11-30 23:12:48 +0100

    Added Completion State

--- script.js
+++ script.js
@@ -1,40 +1,53 @@
 const theme = document.querySelector(".svg");
 const body = document.querySelector("body");
 const form = document.querySelector("form");
-const tasksContainer = document.querySelector(".tasks");
+const tasksContainer = document.getElementById("tasks");
 const input = document.getElementById("input");
+const delBtnAll = document.querySelector(".clear");
 let tasksNumber = document.querySelector(".items-left");
+let tasksFooter = document.getElementById("footer");
 let arrayOfTasks = [];
-
+console.log(delBtnAll)
 //Event Listener
 document.addEventListener("DOMContentLoaded", itemsNumber)
-//submit task
 form.addEventListener("submit", addTask);
+theme.addEventListener("click", themeToggle);
+tasksContainer.addEventListener("click", deleteOrDone );
+tasksFooter.addEventListener("click", completionState);
+delBtnAll.addEventListener("click", delAll)
 
-//change the theme
-theme.addEventListener("click", function (e) {
+//Function
+
+//Delete All Tasks 
+function delAll(){
+  let tasks = tasksContainer.children;
+[...tasks].forEach(task => {
+  task.classList.contains("task") ? tasksContainer.removeChild(task): tasksContainer;
+  itemsNumber()
+})
+}
+// Theme function
+function themeToggle(e){
   if (e.currentTarget.classList.contains("svg")) {
     if (body.classList.contains("theme")) {
       body.classList.toggle("light");
     }
   }
-});
-//State Of the task (Delete or Done)
-tasksContainer.addEventListener("click", function(task){
-const todo = task.target ;
-
-//Delete Todo 
-if(todo.classList.contains("delete")){
-  tasksContainer.removeChild(todo.parentNode);
-  itemsNumber();
 }
-//Finished todo
-if(todo.classList.contains("check")){
-  todo.parentNode.classList.toggle("finished")
-}
-})
-
-//Function
+//State Of the task function's(Delete or Done)
+function deleteOrDone(task){
+  const todo = task.target ;
+  
+  //Delete Todo 
+  if(todo.classList.contains("delete")){
+    tasksContainer.removeChild(todo.parentNode);
+    itemsNumber();
+  }
+  //Finished todo
+  if(todo.classList.contains("check")){
+    todo.parentNode.classList.toggle("finished")
+  }
+  }
 
 function addTask(e) {
   e.preventDefault();
@@ -60,10 +73,9 @@ function addTask(e) {
           
           <button class="delete"><svg xmlns="http://www.w3.org/2000/svg" width="18" height="18"><path fill="#494C6B" fill-rule="evenodd" d="M16.97 0l.708.707L9.546 8.84l8.132 8.132-.707.707-8.132-8.132-8.132 8.132L0 16.97l8.132-8.132L0 .707.707 0 8.84 8.132 16.971 0z"/></svg></button>
     `;
-  //Footer of taskConrtainer
-  let footer = document.getElementById("footer");
+
   //Insert The task Before the footer
-  tasksContainer.insertBefore(task, footer);
+  tasksContainer.insertBefore(task, tasksFooter);
   //Empty tha input value
   input.value = "";
 }
@@ -73,4 +85,44 @@ itemsNumber()
 
 function itemsNumber(){
   tasksNumber.innerText = `${tasksContainer.childElementCount - 1} items left`
+  if(tasksContainer.childElementCount === 1){
+    //Edit Footer's Border-raduis
+    tasksFooter.className = "zero-items";
+  }
 }
+
+// Display items depend of its state
+ 
+function completionState(e){
+const todos = tasksContainer.children;
+let stateColor = e.target
+Array.from(todos).forEach(todo => {
+  switch(e.target.className){
+  case 'all' :
+if(todo.classList.contains("task")){
+  todo.style.display = "flex";
+  stateColor.style.color = "#3a7bfd"
+}
+break;
+case 'active' :
+  if(!todo.classList.contains("finished") || todo.id == 'footer'){
+    todo.style.display = "flex";
+    stateColor.style.color = "#3a7bfd"
+  } else {
+    todo.style.display = "none";
+  }
+  break;
+case 'completed' :
+  if(todo.classList.contains("finished") || todo.id == 'footer'){
+    todo.style.display = "flex";
+    stateColor.style.color = "#3a7bfd"
+  } else {
+    todo.style.display = "none";
+  }
+  break;
+  
+}
+});
+
+}
+
